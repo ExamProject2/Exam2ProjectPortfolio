ad88d7f9f68faac5badad273c762905931636261
anastasia-sl <anastasiaslynko2206@gmail.com>
2023-04-15 13:08:01 +0300

    button

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Button from "../atoms/button";
+import './App.css';
+
+function App() {
+  return (
+   <Button backgrndColor='orange' title='dojkdcnk'>
+  );
+}
+
+export default App;
